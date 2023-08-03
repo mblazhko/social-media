1a11435253632048bc6a7078877bf531f164f8e7
neostyle88 <makssimm1@gmail.com>
2023-08-03 17:24:11 +0200

    add actions to "me" path

--- user/urls.py
+++ user/urls.py
@@ -14,12 +14,24 @@ from user.views import (
 router = routers.DefaultRouter()
 router.register("users", UserViewSet)
 
+
 app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("me/", ManageUserView.as_view(), name="manage"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path(
+        "me/",
+        ManageUserView.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "post": "upload_image",
+            }
+        ),
+        name="manage",
+    ),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("", include(router.urls)),
 ]
