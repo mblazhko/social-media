cea9913fffd1159c7e75906c0b8a8706e5dfe682
neostyle88 <makssimm1@gmail.com>
2023-08-03 19:32:36 +0200

    add tag, post, post_image serializers

---
+++ content/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+
+from content.models import Tag, PostImage, Post
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ("id", "name")
+
+
+class PostImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostImage
+        fields = ("id", "post", "image")
+
+
+class PostSerializer(serializers.ModelSerializer):
+    images = PostImageSerializer(many=True, read_only=True)
+    uploaded_images = serializers.ListField(
+        child=serializers.ImageField(allow_empty_file=False, use_url=False),
+        write_only=True,
+    )
+    hashtags = TagSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = (
+            "id",
+            "owner",
+            "content",
+            "created_at",
+            "liked_by",
+            "hashtags",
+            "images",
+            "uploaded_images",
+        )
+
+    def create(self, validated_data):
+        uploaded_images = validated_data.pop("uploaded_images")
+        product = Post.objects.create(**validated_data)
+
+        for image in uploaded_images:
+            PostImage.objects.create(product=product, image=image)
+
+        return product
