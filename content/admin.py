92d47e8d34925ba50c0fec732e2792051f31e7da
neostyle88 <makssimm1@gmail.com>
2023-08-04 13:55:25 +0200

    register content's models in admin.py

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from content.models import Post, Tag, PostImage
+
+admin.site.register(Post)
+admin.site.register(Tag)
+admin.site.register(PostImage)
